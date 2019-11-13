6593b2cf02c956b47eb6e45df5cee50a4b03c06c
Ben Habermeyer <46455195+BenHabermeyer@users.noreply.github.com>
2019-11-13 12:49:07 -0500

    Add files via upload

---
+++ testscript.py
@@ -0,0 +1,9 @@
+#Testing script just to make sure PyCharm works
+class Yo(object):
+
+    def __init__(self):
+        print("yo")
+
+if __name__ == '__main__':
+    self = Yo()
+    print("hello world")
